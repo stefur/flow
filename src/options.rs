4c5c7637018a6acad84d340e1583bf3b508522e9
stefur <stefan@furne.net>
2023-12-15 22:33:17 +0100

    Added feature to optionally skip unoccupied tags when using `cycle-tags`
    The user can now append the flag `-o`or `--occupied` to the `cycle-tags` command in order to cycle the tagmask over only occupied tags.

--- src/options.rs
+++ src/options.rs
@@ -5,6 +5,7 @@ USAGE:
   flow [COMMAND] [ARGS]
 FLAGS:
   -h, --help            Prints help information
+  -o, --occupied        This flag can be appended to the cycle-tags command to only cycle through occupied tags.
 COMMAND:
   cycle-tags            Takes two arguments. Direction (next or previous) and an optional number of available tags (Default: 9).
   toggle-tags           Toggle previous tags if selected tags already focused.
@@ -20,6 +21,7 @@ pub enum Arguments {
     CycleTags {
         direction: String,
         n_tags: Option<u8>,
+        skip_unoccupied: bool,
     },
     ToggleTags {
         to_tags: u32,
@@ -37,6 +39,7 @@ pub fn parse_args() -> Result<Arguments, Box<dyn std::error::Error>> {
         Some("cycle-tags") => Ok(Arguments::CycleTags {
             direction: pargs.free_from_str()?,
             n_tags: pargs.opt_free_from_str()?,
+            skip_unoccupied: pargs.contains(["-o", "--occupied"]),
         }),
         Some("toggle-tags") => Ok(Arguments::ToggleTags {
             to_tags: pargs.free_from_str()?,
